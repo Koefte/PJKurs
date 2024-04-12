99d110d813edd0e7a426f8311f8a806a20b3e2e9
Henk van Wersch <henkvanwersch123@gmail.com>
2024-04-13 01:09:55 +0200

    Add checking if user exists in db

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const { request } = require('http');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 function hasAllKeys(objToCheck, keysObj) {
   return Object.keys(keysObj).every(key => objToCheck.hasOwnProperty(key));
@@ -19,35 +20,51 @@ const acc = {
   passwort:"passwort"
 } 
 
+const entry = {
+  email:"email.com",
+  passwort:"passwort"
+}
+
+
 // POST request handler
 app.post('/api/users', (req, res) => {
   const requestData = req.body;
-  if(!hasAllKeys(acc,requestData) ){
-    res.status(400).json({message:"Wrong data"})
-    return;
-  }
-
-  try {
-    const existingData = fs.readFileSync('users.json', 'utf-8');
-    const parsedData = JSON.parse(existingData);
-    for(let user of parsedData){
-      console.log(user)
-      if(user.email == requestData.email){
-        console.log(`Found duplicate: ${requestData}`)
-        res.status(400).json({message:"No duplicates allowed, one user per email"})
-        return
+  if(hasAllKeys(requestData,acc) ){
+    try {
+      const existingData = fs.readFileSync('users.json', 'utf-8');
+      const parsedData = JSON.parse(existingData);
+      for(let user of parsedData){
+        console.log(user)
+        if(user.email == requestData.email){
+          console.log(`Found duplicate: ${requestData}`)
+          res.status(400).json({message:"No duplicates allowed, one user per email"})
+          return
+        }
       }
+      parsedData.push(requestData);
+      fs.writeFileSync('users.json', JSON.stringify(parsedData, null, 2), 'utf-8');
+      console.log('Data appended to users.json');
+    } catch (error) {
+      console.error('Error appending data to users.json:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
     }
-    parsedData.push(requestData);
-    fs.writeFileSync('users.json', JSON.stringify(parsedData, null, 2), 'utf-8');
-    console.log('Data appended to users.json');
-  } catch (error) {
-    console.error('Error appending data to users.json:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+  
+  }
+  else if(hasAllKeys(entry,requestData)){
+    let existingData = fs.readFileSync('users.json', 'utf-8');
+    existingData = JSON.parse(existingData)
+    if(existingData.map(data => JSON.stringify([data.email, data.passwort])).includes(JSON.stringify(Object.values(requestData)))){
+      res.status(200).json({message:"Exists"})
+    }
+    else{
+      res.status(500).json({message:"Doesnt exist"})
+    }
     return;
   }
 
-  res.status(200).json({ message: 'POST request received successfully' });
+
+  res.status(400).json({ error: 'Wrong data' });
 });
 
 app.listen(port, () => {
@@ -60,6 +77,7 @@ app.get('/api/users', (req, res) => {
     const jsonData = JSON.parse(data);
 
     res.status(200).json(jsonData);
+    console.log("Succesfully received get request")
   } catch (error) {
     console.error('Error reading users.json:', error);
     res.status(500).json({ error: 'Internal Server Error' });
