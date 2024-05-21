c03d0f7fa5a41907e0bd216b905e098cccd2f169
anb3li <anbeli260907@gmail.com>
2024-05-21 18:25:40 +0200

    navbar

--- AndroidApp/app/src/main/java/de/anbeli/dronedelivery/MainActivity.java
+++ AndroidApp/app/src/main/java/de/anbeli/dronedelivery/MainActivity.java
@@ -9,6 +9,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -18,15 +21,39 @@ import de.anbeli.dronedelivery.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        binding.bottomNavigationView.setOnItemSelectedListener(menuItem -> {
+            if(menuItem.getItemId() == R.id.nav_deliveries)
+                replace_fragment(new DeliveryFragment());
+            else if(menuItem.getItemId() == R.id.nav_contacts)
+                replace_fragment(new ContactFragment());
+            else if(menuItem.getItemId() == R.id.nav_drones)
+                replace_fragment(new DroneFragment());
+            else if(menuItem.getItemId() == R.id.nav_settings)
+                replace_fragment(new SettingsFragment());
+
+            return true;
+        });
+    }
+
+    private void replace_fragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
